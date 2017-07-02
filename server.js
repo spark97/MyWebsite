d66ba0b22ae27e5e38de3c1e3827fd718110bfa6
= <=>
2017-07-02 15:09:29 +0530

    Add about me

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+
+app.use(express.static('./resources/bootstrap/js'));
+app.use(express.static('./resources/bootstrap/css'));
+app.use(express.static('./resources/stylesheets'));
+app.use(express.static('./resources/js'));
+app.use(express.static('./resources/image'));
+
+
+app.get('/',function(request,respone){
+    respone.sendFile(path.join(__dirname+"/views/index.html"));
+});
+
+app.get('/resume',function(request,response){
+    alert('Resume');
+});
+
+
+app.listen(2500);
